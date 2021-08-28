2003be0b3841f5d1e043f3d9f31bcfbbce55f41b
Bill Tsek <billtsekev@gmail.com>
2021-08-29 00:28:38 +0300

    Version 2 of count-chars.c

--- count-chars.c
+++ count-chars.c
@@ -3,9 +3,9 @@
 /* count characters in input */
 int main() {
 
-	long nc = 0;
-	while (getchar() != EOF) {
-		nc++;
-	}
-	printf("%ld\n", nc);
+	double nc;
+	for (nc=0; getchar() != EOF; nc++)
+		;
+		
+	printf("%.0f\n", nc);
 }

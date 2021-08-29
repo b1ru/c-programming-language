1ffcf8fe9d645155fb19f3b2b1e04ffdcc925e0b
Bill Tsek <billtsekev@gmail.com>
2021-08-29 15:44:03 +0300

    Update create-readme.c

--- create-readme.c
+++ create-readme.c
@@ -4,8 +4,9 @@
 int main()
 {
 	int exercises[] = {-1, 24, 10, 6, 13, 20, 6, 9, 8};
+	printf("== Progress\n\n");
 	for (int chapter = 1; chapter <= CHAPTERS; chapter++) {
-		printf("== Chapter %d\n", chapter);
+		printf("=== Chapter %d\n", chapter);
 		for (int exercise = 1; exercise <= exercises[chapter]; exercise++) {
 			printf("* [ ] Exercise %d.%d\n", chapter, exercise);
 		}

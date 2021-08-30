7e441a1b86ec5b20c93b5f086c41bf23a030dcbb
Bill Tsek <billtsekev@gmail.com>
2021-08-30 13:39:15 +0300

    Add exercises 11 and 12

---
+++ exercises/chapter-1/exercise-12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+	int c;
+	while ((c=getchar()) != EOF) {
+		if (c=='\n')
+			putchar('\n');
+		if (c==' ' || c=='\t' || c=='\n') {
+			while ((c=getchar()) != EOF) {
+				if (c!=' ' && c!='\t' && c!='\n')
+					break;
+			}
+			putchar('\n');
+			putchar(c);
+		} else {
+			putchar(c);
+		}
+	}
+}

c7dc0b3c1b31be98861338f4b73027480a760205
Bill Tsek <billtsekev@gmail.com>
2021-08-30 13:52:27 +0300

    Change README from asciidoc to markdown

--- create-readme.c
+++ create-readme.c
@@ -4,11 +4,11 @@
 int main()
 {
 	int exercises[] = {-1, 24, 10, 6, 13, 20, 6, 9, 8};
-	printf("== Progress\n\n");
+	printf("## Progress\n");
 	for (int chapter = 1; chapter <= CHAPTERS; chapter++) {
-		printf("=== Chapter %d\n", chapter);
+		printf("### Chapter %d\n", chapter);
 		for (int exercise = 1; exercise <= exercises[chapter]; exercise++) {
-			printf("* [ ] Exercise %d.%d\n", chapter, exercise);
+			printf("- [ ] Exercise %d.%d\n", chapter, exercise);
 		}
 		printf("\n");
 	}
